8f5d397cfbc9a252b80d4cb4be53e5cdc8236c8c
ohlunasa <me@lunasa.dev>
2026-08-18 18:44:40 +0530

    jvm: add bindings for makeRaster

--- src/main/kotlin/org/skialin/Surface.kt
+++ src/main/kotlin/org/skialin/Surface.kt
@@ -16,6 +16,11 @@ class Surface private constructor(ptr: Long) : Managed(ptr, SurfaceNative::nRele
             val ptr = SurfaceNative.nMakeRasterN32Premul(width, height)
             return if (ptr == 0L) null else Surface(ptr)
         }
+
+        fun makeRaster(info: ImageInfo): Surface? {
+            val ptr = SurfaceNative.nMakeRaster(info.nativePtr)
+            return if (ptr == 0L) null else Surface(ptr)
+        }
     }
 }
 
@@ -25,6 +30,7 @@ private object SurfaceNative {
     }
 
     external fun nMakeRasterN32Premul(width: Int, height: Int): Long
+    external fun nMakeRaster(infoPtr: Long): Long
     external fun nRelease(ptr: Long)
     external fun nGetCanvas(ptr: Long): Long
     external fun nMakeImageSnapshot(ptr: Long): Long
